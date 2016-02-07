c5b0f0039535923ec283c52a847a628e14334c0a
jan-molak <jan.molak@gmail.com>
2016-02-07 19:38:19 +0000

    Cleaned up the examples related to issue #9

--- src/it/print-version-multi-module/verify.groovy
+++ src/it/print-version-multi-module/verify.groovy
@@ -1,6 +1,6 @@
 new GroovyShell().parse(new File('./src/it/integration_tests.groovy')).with {
 
-    exportedVersion = parseVersionFile("./target/it/print-version-multi-module", 'maven.version')
+    exported_version = read(basedir, 'project.version')
 
-    expect exportedVersion, "1.0.0-SNAPSHOT", "version exported via 'version' goal"
+    expect exported_version, "1.0.0-SNAPSHOT", "version exported via 'version' goal"
 }
